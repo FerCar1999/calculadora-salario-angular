ebf41957df48702da86af3224a55678430b0da1b
bernaluis <bernal.lrm@hotmail.com>
2023-09-23 21:24:57 -0600

    Calculo y display de valores de renta

--- src/app/calculadora-salario/calculadora-salario.component.ts
+++ src/app/calculadora-salario/calculadora-salario.component.ts
@@ -15,10 +15,8 @@ export class CalculadoraSalarioComponent{
   salario_liquido: number = 0.00;
   //dependiendo del tipo de salario sera el tipo de calculo para la renta
   //1=semanal, 2=quincenal, 3=mensual
-  tipoSalario:number=0
-  //en base a esta variable se calcula si aplica o no al descuento de renta segun el MH
-  //formula es salario base-descuento afp-descuento isss
-  salario_nominal:number=this.salario-this.afp-this.isss
+  tipoSalario:string="Mensual"
+  salario_nominal:number=0
   //funcion para verificar el input si es numero o no
   isNumber(): boolean {
     const regex = /^\d+(\.\d{1,2})?$/;
@@ -39,6 +37,7 @@ export class CalculadoraSalarioComponent{
   }
 
   //funcion para calculo de renta, devuelve un numero que debe ser asignado a la variable renta
+  //tipo salario debe ser 3
   calculoRentaMes():number{
     if(this.salario_nominal>=472.01&&this.salario_nominal<=895.24){
       return (((this.salario_nominal-472)*0.1)+17.67)
@@ -50,6 +49,7 @@ export class CalculadoraSalarioComponent{
       return 0.00
     }
   }
+  //tipo salario debe ser 2
   calculoRentaQuincena():number{
     if(this.salario_nominal>=236.01&&this.salario_nominal<=447.62){
       return (((this.salario_nominal-236)*0.1)+8.83)
@@ -61,6 +61,7 @@ export class CalculadoraSalarioComponent{
       return 0.00
     }
   }
+  //tipo salario debe ser 1
   calculoRentaSemanal():number{
     if(this.salario_nominal>=118.01&&this.salario_nominal<=223.81){
       return (((this.salario_nominal-118)*0.1)+4.42)
@@ -77,8 +78,25 @@ export class CalculadoraSalarioComponent{
   calcularDescuentos(){
     //uso la funcion para permitir el paso o no del valor y verificar si esta correcto
     if(this.handleInput()){
+      //calculo el salario con los descuentos de afp y seguro para poder calcular la renta
+      //formula es salario base-descuento afp-descuento isss
+      this.salario_nominal=this.salario-this.afp-this.isss
       //aca van las funciones de calculo, entra aca si el formato esta correcto
-      console.log("Formato correcto")
+      //con este switch verificamos el tipo de salario para ver que calculo de renta le aplica
+      switch(this.tipoSalario){
+        //si el salario es semanal
+        case "Semanal":
+          this.renta=this.roundToTwoDecimals(this.calculoRentaSemanal())
+          break
+        //si el salario es quincenal
+        case "Quincenal":
+          this.renta=this.roundToTwoDecimals(this.calculoRentaQuincena())
+          break
+        //si el salario es mensual
+        case "Mensual":
+          this.renta=this.roundToTwoDecimals(this.calculoRentaMes())
+          break
+      }
       
     }else{
       console.log("Formato incorrecto")
